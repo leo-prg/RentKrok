ae62f61269bc3c7c1b6fd56961683e13b438f452
leo-prg <leo.prg@gmail.com>
2020-01-10 16:49:25 +0300

    Cors fix, edit by ID

--- RentAPI/Controllers/ObjectsController.cs
+++ RentAPI/Controllers/ObjectsController.cs
@@ -6,9 +6,12 @@ using System.Net.Http;
 using System.Web.Http;
 using RentDB;
 using RentLibrary;
+using System.Web.Http.Cors;
 
 namespace RentAPI.Controllers
 {
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class ObjectsController : ApiController
     {
         Lazy<DBObject> dbo = new Lazy<DBObject>();
--- RentAPI/Controllers/RentersController.cs
+++ RentAPI/Controllers/RentersController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using RentDB;
 using RentLibrary;
 
 namespace RentAPI.Controllers
 {
-    
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
 
     public class RentersController : ApiController
     {
