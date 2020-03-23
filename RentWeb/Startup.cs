3cdc80022afe7b8d891114aa8b0847d1eecceb85
Leo <leo.prg@gmail.com>
2020-03-23 08:08:54 +0300

    LocalStorage working

--- RentWeb/Startup.cs
+++ RentWeb/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Blazored.LocalStorage;
 
 namespace RentWeb
 {
@@ -7,7 +8,7 @@ namespace RentWeb
     {
         public void ConfigureServices(IServiceCollection services)
         {
-         
+            services.AddBlazoredLocalStorage();
         }
 
         public void Configure(IComponentsApplicationBuilder app)
